4cc2f639243cac8fcf24cd2807a4b5baa93ea26f
hyunmin0317 <hyunmin-choi@naver.com>
2025-01-17 20:23:11 +0900

    chore: 의존성 설정 (#1)

--- coupon-core/build.gradle.kts
+++ coupon-core/build.gradle.kts
@@ -1,26 +1,18 @@
-plugins {
-    java
-    id("org.springframework.boot") version "3.4.1"
-    id("io.spring.dependency-management") version "1.1.7"
-}
+val bootJar: org.springframework.boot.gradle.tasks.bundling.BootJar by tasks
 
-group = "com.example"
-version = "0.0.1-SNAPSHOT"
-
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
-    }
-}
+bootJar.enabled = false
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+    implementation("com.fasterxml.jackson.core:jackson-databind")
+    implementation("org.redisson:redisson-spring-boot-starter:3.16.4")
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("com.github.ben-manes.caffeine:caffeine")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
