09964e2c38e414449128d0d68118b6af48bfbeef
hyunmin0317 <hyunmin-choi@naver.com>
2025-02-07 19:10:31 +0900

    feat: 쿠폰 엔티티 발급 기능 (#3)

---
+++ core/src/main/java/com/example/couponcore/exception/CouponIssueException.java
@@ -0,0 +1,17 @@
+package com.example.couponcore.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CouponIssueException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+    private final String message;
+
+    @Override
+    public String getMessage() {
+        return "[%s] %s".formatted(errorCode, message);
+    }
+}
