c8196dd35c125c0a95476b8e9f18aed5c291764f
hyunmin0317 <hyunmin-choi@naver.com>
2025-02-07 19:10:31 +0900

    feat: 쿠폰 엔티티 발급 기능 (#3)

--- core/src/main/java/com/example/couponcore/model/Coupon.java
+++ core/src/main/java/com/example/couponcore/model/Coupon.java
@@ -1,5 +1,7 @@
 package com.example.couponcore.model;
 
+import com.example.couponcore.exception.CouponIssueException;
+import com.example.couponcore.exception.ErrorCode;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -40,4 +42,23 @@ public class Coupon extends BaseTimeEntity {
 
     @Column(nullable = false)
     private LocalDateTime dateIssueEnd;
+
+    public boolean availableIssueQuantity() {
+        return totalQuantity == null || totalQuantity > issuedQuantity;
+    }
+
+    public boolean availableIssueDate() {
+        LocalDateTime now = LocalDateTime.now();
+        return dateIssueStart.isBefore(now) && getDateIssueEnd().isAfter(now);
+    }
+
+    public void issue() {
+        if (!availableIssueQuantity()) {
+            throw new CouponIssueException(ErrorCode.INVALID_COUPON_ISSUE_QUANTITY, "발급 가능한 수량을 초과합니다. total : %s, issued: %s".formatted(totalQuantity, issuedQuantity));
+        }
+        if (!availableIssueDate()) {
+            throw new CouponIssueException(ErrorCode.INVALID_COUPON_ISSUE_DATE, "발급 가능한 일자가 아닙니다. request: %s, issueStart: %s, issueEnd: %s".formatted(LocalDateTime.now(), dateIssueStart, dateIssueEnd));
+        }
+        issuedQuantity++;
+    }
 }
