e085819de706dd49f8aad4c317f5046bb439e356
hyunmin0317 <hyunmin-choi@naver.com>
2025-02-10 18:53:23 +0900

    chore: Swagger 의존성 수정 (#3)

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -2,6 +2,7 @@ dependencies {
     implementation(project(":core"))
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
