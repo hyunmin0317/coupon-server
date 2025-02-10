998899edb288289950a2e9dd926954f7ed04a222
hyunmin0317 <hyunmin-choi@naver.com>
2025-02-10 19:17:04 +0900

    feat: 쿠폰 발급 API 예외처리 (#3)

---
+++ api/src/main/java/com/coupon/api/CouponControllerAdvice.java
@@ -0,0 +1,15 @@
+package com.coupon.api;
+
+import com.coupon.api.controller.dto.CouponIssueResponseDto;
+import com.coupon.core.exception.CouponIssueException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CouponControllerAdvice {
+
+    @ExceptionHandler(CouponIssueException.class)
+    public CouponIssueResponseDto couponIssueExceptionHandler(CouponIssueException exception) {
+        return new CouponIssueResponseDto(false, exception.getErrorCode().message);
+    }
+}
