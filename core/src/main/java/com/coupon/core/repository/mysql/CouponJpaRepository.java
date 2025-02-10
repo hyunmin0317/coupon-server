2d2bfab321b291192280ec37ce315ec16b386e8e
hyunmin0317 <hyunmin-choi@naver.com>
2025-02-10 21:02:53 +0900

    feat: 동시성 이슈 해결 - mysql lock (#3)

--- core/src/main/java/com/coupon/core/repository/mysql/CouponJpaRepository.java
+++ core/src/main/java/com/coupon/core/repository/mysql/CouponJpaRepository.java
@@ -1,8 +1,16 @@
 package com.coupon.core.repository.mysql;
 
 import com.coupon.core.model.Coupon;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface CouponJpaRepository extends JpaRepository<Coupon, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT c FROM Coupon c WHERE c.id = :id")
+    Optional<Coupon> findCouponWithLock(long id);
 }
