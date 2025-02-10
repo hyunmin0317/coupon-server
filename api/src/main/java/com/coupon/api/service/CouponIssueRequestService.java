a8bdbd2c3be06bd7964b71f472ab10fec5cb8180
hyunmin0317 <hyunmin-choi@naver.com>
2025-02-10 20:12:20 +0900

    feat: 동시성 이슈 해결 - redis lock (#3)

--- api/src/main/java/com/coupon/api/service/CouponIssueRequestService.java
+++ api/src/main/java/com/coupon/api/service/CouponIssueRequestService.java
@@ -1,6 +1,7 @@
 package com.coupon.api.service;
 
 import com.coupon.api.controller.dto.CouponIssueRequestDto;
+import com.coupon.core.component.DistributeLockExecutor;
 import com.coupon.core.service.CouponIssueService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,9 +13,24 @@ import org.springframework.stereotype.Service;
 public class CouponIssueRequestService {
 
     private final CouponIssueService couponIssueService;
+    private final DistributeLockExecutor distributeLockExecutor;
 
     public void issueRequestV1(CouponIssueRequestDto requestDto) {
-        couponIssueService.issue(requestDto.couponId(), requestDto.userId());
+        couponIssueService.issueV1(requestDto.couponId(), requestDto.userId());
+        log.info("쿠폰 발급 완료. couponId: %s, userId: %s".formatted(requestDto.couponId(), requestDto.userId()));
+    }
+
+    public void issueRequestV2(CouponIssueRequestDto requestDto) {
+        synchronized (this) {
+            couponIssueService.issueV1(requestDto.couponId(), requestDto.userId());
+        }
+        log.info("쿠폰 발급 완료. couponId: %s, userId: %s".formatted(requestDto.couponId(), requestDto.userId()));
+    }
+
+    public void issueRequestV3(CouponIssueRequestDto requestDto) {
+        distributeLockExecutor.execute("lock_%s".formatted(requestDto.couponId()), 10000, 10000, () -> {
+            couponIssueService.issueV1(requestDto.couponId(), requestDto.userId());
+        });
         log.info("쿠폰 발급 완료. couponId: %s, userId: %s".formatted(requestDto.couponId(), requestDto.userId()));
     }
 }
