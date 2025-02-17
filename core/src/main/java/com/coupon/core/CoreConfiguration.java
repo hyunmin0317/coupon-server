f5a9376342e9ab2c271bd3673dc72dc736457dd9
hyunmin0317 <hyunmin-choi@naver.com>
2025-02-17 20:01:58 +0900

    feat: 캐시 설정 (#5)

--- core/src/main/java/com/coupon/core/CoreConfiguration.java
+++ core/src/main/java/com/coupon/core/CoreConfiguration.java
@@ -1,9 +1,11 @@
 package com.coupon.core;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableCaching
 @EnableJpaAuditing
 @ComponentScan
 @EnableAutoConfiguration
