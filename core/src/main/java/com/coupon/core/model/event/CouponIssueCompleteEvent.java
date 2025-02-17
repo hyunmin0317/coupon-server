6bb733f92d0f733f472525bda015e174153913e8
hyunmin0317 <hyunmin-choi@naver.com>
2025-02-17 22:03:43 +0900

    feat: 쿠폰 발급 기능 로컬 캐시 적용 (#6)

---
+++ core/src/main/java/com/coupon/core/model/event/CouponIssueCompleteEvent.java
@@ -0,0 +1,5 @@
+package com.coupon.core.model.event;
+
+public record CouponIssueCompleteEvent(long couponId) {
+    
+}
--- core/src/main/java/com/coupon/core/service/CouponIssueService.java
+++ core/src/main/java/com/coupon/core/service/CouponIssueService.java
@@ -4,10 +4,12 @@ import com.coupon.core.exception.CouponIssueException;
 import com.coupon.core.exception.ErrorCode;
 import com.coupon.core.model.Coupon;
 import com.coupon.core.model.CouponIssue;
+import com.coupon.core.model.event.CouponIssueCompleteEvent;
 import com.coupon.core.repository.mysql.CouponIssueJpaRepository;
 import com.coupon.core.repository.mysql.CouponIssueRepository;
 import com.coupon.core.repository.mysql.CouponJpaRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +20,7 @@ public class CouponIssueService {
     private final CouponJpaRepository couponJpaRepository;
     private final CouponIssueJpaRepository couponIssueJpaRepository;
     private final CouponIssueRepository couponIssueRepository;
+    private final ApplicationEventPublisher applicationEventPublisher;
 
     @Transactional
     public void issueV1(long couponId, long userId) {
@@ -31,6 +34,7 @@ public class CouponIssueService {
         Coupon coupon = findCouponWithLock(couponId);
         coupon.issue();
         saveCouponIssue(couponId, userId);
+        publishCouponEvent(coupon);
     }
 
     @Transactional(readOnly = true)
@@ -57,6 +61,12 @@ public class CouponIssueService {
         CouponIssue issue = couponIssueRepository.findFirstCouponIssue(couponId, userId);
         if (issue != null) {
             throw new CouponIssueException(ErrorCode.DUPLICATED_COUPON_ISSUE, "이미 발급된 쿠폰입니다. user_id: %s, coupon_id: %s".formatted(userId, couponId));
+        }
+    }
+
+    private void publishCouponEvent(Coupon coupon) {
+        if (coupon.isIssueComplete()) {
+            applicationEventPublisher.publishEvent(new CouponIssueCompleteEvent(coupon.getId()));
         }
     }
 }
