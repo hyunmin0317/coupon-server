e00aadf1e93e3cefde8f1553e29b22df5deb232c
hyunmin0317 <hyunmin-choi@naver.com>
2025-02-17 20:02:21 +0900

    feat: 캐시 적용 (#5)

---
+++ core/src/main/java/com/coupon/core/repository/redis/dto/CouponRedisEntity.java
@@ -0,0 +1,48 @@
+package com.coupon.core.repository.redis.dto;
+
+import com.coupon.core.exception.CouponIssueException;
+import com.coupon.core.exception.ErrorCode;
+import com.coupon.core.model.Coupon;
+import com.coupon.core.model.CouponType;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+import java.time.LocalDateTime;
+
+public record CouponRedisEntity(
+        Long id,
+        CouponType couponType,
+        Integer totalQuantity,
+
+        @JsonSerialize(using = LocalDateTimeSerializer.class)
+        @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+        LocalDateTime dateIssueStart,
+
+        @JsonSerialize(using = LocalDateTimeSerializer.class)
+        @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+        LocalDateTime dateIssueEnd
+) {
+
+    public CouponRedisEntity(Coupon coupon) {
+        this(
+                coupon.getId(),
+                coupon.getCouponType(),
+                coupon.getTotalQuantity(),
+                coupon.getDateIssueStart(),
+                coupon.getDateIssueEnd()
+        );
+    }
+
+    private boolean availableIssueDate() {
+        LocalDateTime now = LocalDateTime.now();
+        return dateIssueStart.isBefore(now) && dateIssueEnd.isAfter(now);
+    }
+
+    public void checkIssuableCoupon() {
+        if (!availableIssueDate()) {
+            throw new CouponIssueException(ErrorCode.INVALID_COUPON_ISSUE_DATE, "발급 가능한 일자가 아닙니다. request: %s, issueStart: %s, issueEnd: %s".formatted(id, dateIssueStart, dateIssueEnd));
+        }
+    }
+}
---
+++ core/src/main/java/com/coupon/core/service/CouponCacheService.java
@@ -0,0 +1,20 @@
+package com.coupon.core.service;
+
+import com.coupon.core.model.Coupon;
+import com.coupon.core.repository.redis.dto.CouponRedisEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CouponCacheService {
+
+    private final CouponIssueService couponIssueService;
+
+    @Cacheable(cacheNames = "coupon")
+    public CouponRedisEntity getCouponCache(long couponId) {
+        Coupon coupon = couponIssueService.findCoupon(couponId);
+        return new CouponRedisEntity(coupon);
+    }
+}
