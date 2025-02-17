8720caf49c2030f90286683d85b38a60febed585
hyunmin0317 <hyunmin-choi@naver.com>
2025-02-17 21:30:13 +0900

    feat: 쿠폰 발급 서버 구현 (#6)

---
+++ consumer/src/main/java/com/coupon/consumer/listener/CouponIssueListener.java
@@ -0,0 +1,50 @@
+package com.coupon.consumer.listener;
+
+import com.coupon.core.repository.redis.RedisRepository;
+import com.coupon.core.repository.redis.dto.CouponIssueRequest;
+import com.coupon.core.service.CouponIssueService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import static com.coupon.core.util.CouponRedisUtils.getIssueRequestQueueKey;
+
+@Slf4j
+@Component
+@EnableScheduling
+@RequiredArgsConstructor
+public class CouponIssueListener {
+
+    private final RedisRepository redisRepository;
+    private final CouponIssueService couponIssueService;
+    private final String issueRequestQueueKey = getIssueRequestQueueKey();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Scheduled(fixedDelay = 1000)
+    public void issue() throws JsonProcessingException {
+        log.info("listen...");
+        while (existCouponIssueTarget()) {
+            CouponIssueRequest target = getIssueTarget();
+            log.info("발급 시작 target: " + target);
+            couponIssueService.issueV2(target.couponId(), target.userId());
+            log.info("발급 완료 target: " + target);
+            removeIssuedTarget();
+        }
+    }
+
+    private boolean existCouponIssueTarget() {
+        return redisRepository.lSize(issueRequestQueueKey) > 0;
+    }
+
+    private CouponIssueRequest getIssueTarget() throws JsonProcessingException {
+        return objectMapper.readValue(redisRepository.lIndex(issueRequestQueueKey, 0), CouponIssueRequest.class);
+    }
+
+    private void removeIssuedTarget() {
+        redisRepository.lPop(issueRequestQueueKey);
+    }
+}
